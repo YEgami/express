8caa531ee2448b2884a242cde4162dce00902233
Yuji Egami <yujiegami@MacBook-Air-M3-Yuji.local>
2024-11-09 21:44:59 +0900

    Notesコメント追記

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello'); //追加
+var notesRouter = require('./routes/notes'); //追加第6回
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter); //追加
-
+app.use('/notes', notesRouter); //追加第6回
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
