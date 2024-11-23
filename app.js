010b13b35a90ddcd65a5e179ed082f7fa555d3ca
Yuji Egami <yujiegami@MacBook-Air-M3-Yuji.local>
2024-11-23 16:16:59 +0900

    updates

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello'); //追加
 var notesRouter = require('./routes/notes'); //追加第6回
 var catRouter = require('./routes/cat'); //追加第8回 
+var yesnoRouter = require('./routes/yesno'); //追加第8回レポート用 
+
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter); //追加
 app.use('/notes', notesRouter); //追加第6回
 app.use('/cat', catRouter); //追加第8回
+app.use('/yesno', yesnoRouter); //追加第8回レポート用
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://";
+const uri = "";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
