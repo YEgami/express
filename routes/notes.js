977f10e90dbb05dfa81cbfe7c9de08d3f4ce6f41
Yuji Egami <yujiegami@MacBook-Air-M3-Yuji.local>
2025-01-19 12:33:23 +0900

    notes update

--- routes/notes.js
+++ routes/notes.js
@@ -7,13 +7,15 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+router.use(cors());
+
 router.get('/', async (req, res) => {
     //データベースの指定
     const database = client.db('notes');
     const notes = database.collection('notes');
 
     //データベースからデータ取得 id2を取得
-    const query = {id:1};
+    const query = {name:'Yasushi'};
     const note = await notes.findOne(query);
 
     res.json(note);
